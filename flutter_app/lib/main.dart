f58ce1c5aed9fe4241f3ad20375a97ddb036c9f5
Owen Carr <owenjcarr@gmail.com>
2021-03-28 17:44:30 -0500

    base for flutter application

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './screens/login.dart';
+import './screens/home.dart';
+import './screens/change_pass.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RootedWI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChangePassScreen(),
+    );
+  }
+}
---
+++ flutter_app/lib/screens/change_pass.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChangePassScreen extends StatefulWidget {
+  @override
+  _ChangePassScreenState createState() => _ChangePassScreenState();
+}
+
+class _ChangePassScreenState extends State<ChangePassScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Change Pass Screen')
+    );
+  }
+}
