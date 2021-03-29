0ef0014981f4f920e9f7777edaf3ca02e2dc5ae1
Owen Carr <owenjcarr@gmail.com>
2021-03-29 02:22:12 -0500

    updated app screen templates

--- flutter_app/lib/screens/change_pass.dart
+++ flutter_app/lib/screens/change_pass.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 
-class ChangePassScreen extends StatefulWidget {
-  @override
-  _ChangePassScreenState createState() => _ChangePassScreenState();
-}
-
-class _ChangePassScreenState extends State<ChangePassScreen> {
+class ChangePassScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Change Pass Screen')
+      child: Text('Change Password Screen')
     );
   }
 }
--- flutter_app/lib/screens/home.dart
+++ flutter_app/lib/screens/home.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
 
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Home')
+      child: Text('Home Screen')
     );
   }
 }
